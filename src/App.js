e8289b15cd56b2076c2831fe47780233b90a5927
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-05-06 10:25:21 -0700

    static portfolio: Create code to generate blog posts

--- src/App.js
+++ src/App.js
@@ -2,11 +2,49 @@ import React, {Component} from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+// Begin blog stuff
+
+import 'github-markdown-css'
+import {Link} from 'react-router-dom'
+
+import BlogPreview from './layouts/BlogPreview'
+// import { Link, IndexRoute, Route, BrowserRouter } from 'react-router'
+
+// eslint-disable-next-line import/no-webpack-loader-syntax
+const webpackRequireContext = require.context(
+  '!markdown-with-front-matter!./_posts',
+  false,
+  /\.md$/
+)
+
+const blogs = webpackRequireContext
+  .keys()
+  .map(path => {
+    const postData = webpackRequireContext(path)
+
+    return {
+      ...postData,
+      path: path.slice(1), // remove the `.` at beginning of the path
+      date: new Date(postData.date || Date.now())
+    }
+  })
+  .sort((post1, post2) => post1.date > post2.date)
+
+const BlogList = ({blogs}) => (
+  <ul>
+    {blogs.map(({path, title}) => <li key={path}>{title}</li>)}
+  </ul>
+)
+
+// End blog stuff
+
+console.log(blogs)
+
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        HI BEN.
+        <BlogPreview posts={blogs} />
       </div>
     )
   }
