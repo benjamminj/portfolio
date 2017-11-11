7ba49f4a452cc1bddb23a0c317d0642197da6a25
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-11-11 00:06:44 -0800

    Add `name` key to social media

--- src/constants/social-media.js
+++ src/constants/social-media.js
@@ -10,21 +10,25 @@ export default [
   {
     url: 'https://twitter.com/benjamminj',
     icon: <TwitterLogoIcon />,
-    color: '#1da1f2'
+    color: '#1da1f2',
+    name: 'Twitter'
   },
   {
     url: 'https://github.com/benjaminj6',
     icon: <GithubLogoIcon />,
-    color: 'var(--black)'
+    color: 'var(--black)',
+    name: 'Github'
   },
   {
     url: 'https://www.linkedin.com/in/benjamin-d-johnson',
     icon: <LinkedInLogoIcon />,
-    color: '#0077b5'
+    color: '#0077b5',
+    name: 'LinkedIn'
   },
   {
     url: 'https://medium.com/@benjamin.d.johnson',
     icon: <MediumLogoIcon />,
-    color: 'var(--black)'
+    color: 'var(--black)',
+    name: 'Medium'
   }
 ]
