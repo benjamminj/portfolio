9774293e3ae70e7949401fe4526f086fea1c4a26
Benjamin Johnson <benjamin.johnson@autogravity.com>
2017-11-20 14:49:26 -0800

    Update content for projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,18 +1,24 @@
 export default [
   {
     name: 'Horizon',
-    description: 'This is an app that is super great...I like it a lot',
+    description: `
+      How long till sunset? Till sunrise? With this animated countdown, you'll never have to guess (or do the math) again. Watch as the evening sky changes to reflect the time outdoors.
+    `,
     link: 'https://benjaminj6.github.io/horizon/',
     repo: 'https://github.com/benjaminj6/horizon'
   },
   {
     name: 'Project Euler',
-    description: 'This is an app that is super great...I like it a lot',
+    description: `
+      Project Euler is a set of mathmatical/computer programming riddles that I've been solving in my spare time. Many of these problems are tricky or operate at a large scale, forcing the developer to find elegant & efficient solutions.
+    `,
     repo: 'https://github.com/benjaminj6/project-euler-js'
   },
   {
     name: 'Adze',
-    description: 'This is an app that is super great...I like it a lot',
+    description: `
+      Finally, a CMS for the rest of us. Don't worry about managing pages or way too many projects. Write your blog posts when you want to, and access them anywhere without being tied to a certain frontend platform.
+    `,
     link: 'https://benjaminj6.github.io/adze/',
     repo: 'https://github.com/benjaminj6/adze'
   }
