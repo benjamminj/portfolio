dd19bbfa685eff9b1778b679c03d05b97fe81213
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-11-21 22:40:08 -0800

    optimizing the deployment process

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // TODO -- import this conditionally
 import App from './components/app'
 import './styles/reset.css'
-import './styles/variables.css'
 
 // TODO -- add conditions to exclude from browsers that don't need it
 // Polyfills only on the browser
