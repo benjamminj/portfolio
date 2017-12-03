a23c5800a0b7ab191f42358a876663263c23d8aa
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-12-03 15:40:44 -0800

    remove styled-jsx dependency

--- babel-configs/custom-plugins.js
+++ babel-configs/custom-plugins.js
@@ -1,14 +1,7 @@
-module.exports = (plugins) => {
+module.exports = plugins => {
   // insert rules for styled-jsx in correct order
   const head = plugins.slice(0, 3)
   const tail = plugins.slice(3)
 
-  return [
-    ...head,
-    'styled-jsx/babel',
-    'syntax-flow',
-    'transform-flow-strip-types',
-    ...tail,
-    'styled-jsx/babel' // need to include twice for proper behavior
-  ]
+  return [...head, 'syntax-flow', 'transform-flow-strip-types', ...tail]
 }
