ad9be44cc0b8e88b58cd0045190746b534c8d5d6
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-12-31 16:59:07 -0800

    added GraphQL query for all blog posts

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from 'react'
 
-const BlogLandingPage = () => (
+const BlogLandingPage = ({ data, errors }) => (
   <div>
     <h1>Blog</h1>
 
-    <p>
-      Welcome to the blog! This hasn't been written yet.
-    </p>
+    <p>Welcome to the blog! This hasn't been written yet.</p>
+
+    <pre><code>
+      {JSON.stringify(data, null, 4)}
+    </code></pre>
   </div>
 )
 
 export default BlogLandingPage
+
+export const query = graphql`
+  query PostsQuery {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "//pages/blog//" } }
+      sort: { fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "MM-DD-YYYY")
+          }
+        }
+      }
+    }
+  }
+`
