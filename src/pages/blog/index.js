83d4b28896a3008eec9928c2fd667426e3ee64c5
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2019-01-30 15:36:39 -0800

    Fix query

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -19,7 +19,7 @@ const BlogLandingPage = ({ data, errors }) => {
   }))
 
   const logrocketPosts = allMediumUser.edges[0].node.posts
-    .filter(post => post.homeCollectionId !== LOGROCKET_COLLECTION_ID)
+    .filter(post => post.homeCollectionId === LOGROCKET_COLLECTION_ID)
     .map(
       ({
         updatedAt,
@@ -132,7 +132,7 @@ export const query = graphql`
               readingTime
             }
             uniqueSlug
-            approvedHomeCollectionId
+            homeCollectionId
             previewContent {
               isFullContent
               subtitle
