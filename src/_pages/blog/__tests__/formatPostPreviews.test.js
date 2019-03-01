8eb5d3736574efa9fa517a5328ea86d2775ba15d
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2019-03-01 15:44:26 -0800

    Get tests running ✅

--- src/_pages/blog/__tests__/formatPostPreviews.test.js
+++ src/_pages/blog/__tests__/formatPostPreviews.test.js
@@ -1,5 +1,9 @@
 import formatPostPreviews from '../formatPostPreviews'
+import queryJson from '../__data__/unformattedPostData'
 
 describe('formatPostPreviews', () => {
-  test()
+  test('returns the correct number of posts', () => {
+    const posts = formatPostPreviews(queryJson)
+    expect(posts.length).toEqual(8)
+  })
 })
