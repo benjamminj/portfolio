af2618bd0e09b1337899d1b334124935e745e7d1
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2019-03-01 20:54:14 -0800

    Move posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,7 +12,7 @@ const { createFilePath } = require('gatsby-source-filesystem')
 exports.onCreateNode = ({ node, getNode, actions }) => {
   if (node.internal.type === 'MarkdownRemark') {
     // const fileNode = getNode(node.parent)
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    const slug = createFilePath({ node, getNode, basePath: `posts` })
 
     const { createNodeField } = actions
 
@@ -20,7 +20,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     createNodeField({
       node,
       name: 'slug',
-      value: slug
+      value: 'blog/' + slug
     })
   }
 }
