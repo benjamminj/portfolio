13b6871a5798f551cc0c060fc1e517d5d033ba25
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2019-03-14 21:55:15 -0700

    Refactor head metadata

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
 module.exports.LOGROCKET_URL = 'https://blog.logrocket.com'
 module.exports.LOGROCKET_COLLECTION_ID = '7f3d96429888'
+module.exports.HOMEPAGE =
+  process.env.NODE_ENV === 'production'
+    ? 'https://benjaminjohnson.me'
+    : 'http://localhost:3000'
