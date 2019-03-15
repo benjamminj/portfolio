b967de1ef7a814b05f240ddfc9bbdbb40f327f09
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2019-03-14 22:12:57 -0700

    Try subdomain

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ module.exports.LOGROCKET_URL = 'https://blog.logrocket.com'
 module.exports.LOGROCKET_COLLECTION_ID = '7f3d96429888'
 module.exports.HOMEPAGE =
   process.env.NODE_ENV === 'production'
-    ? 'https://benjaminjohnson.me'
+    ? 'https://www.benjaminjohnson.me'
     : 'http://localhost:3000'
