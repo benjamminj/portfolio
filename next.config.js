92e10150cc59cf62c88ef3545d502a13c3d4ff51
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-06-28 17:48:01 -0700

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withMdxEnhanced = require('next-mdx-enhanced')
 const withOptimizedImages = require('next-optimized-images')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
-module.exports = withMdxEnhanced({
-  fileExtensions: ['mdx', 'md']
-})(withOptimizedImages({}))
+module.exports = withBundleAnalyzer(
+  withMdxEnhanced({
+    fileExtensions: ['mdx', 'md']
+  })(withOptimizedImages({}))
+)
