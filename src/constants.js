11a35458a3708b18082d1dfbb41e6ed7e410ee08
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-07-02 22:13:20 -0700

    remove gatsby code

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports.LOGROCKET_URL = 'https://blog.logrocket.com'
-module.exports.LOGROCKET_COLLECTION_ID = '7f3d96429888'
-module.exports.HOMEPAGE =
-  process.env.NODE_ENV === 'production'
-    ? 'https://www.benjaminjohnson.me'
-    : 'http://localhost:3000'
