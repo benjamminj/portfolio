cc6f23355c8c2fb0444cc7e6f76e91a40647f58c
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-07-29 17:40:23 -0700

    add e2e tests to actions workflow

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'jest-playwright-preset',
   setupFilesAfterEnv: ['./test-utils/setupTests.ts'],
-  // modulePathIgnorePatterns: ['cypress'],
   collectCoverageFrom: ['<rootDir>/src/**']
 }
