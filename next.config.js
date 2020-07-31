61bae0b150a8a9accf61ceaaa7fd3c4344fd69fe
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-07-30 22:45:52 -0700

    fix: add vercel url (#94)

--- next.config.js
+++ next.config.js
@@ -4,18 +4,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-const BASE_URL =
-  process.env.CONTEXT === 'production'
-    ? process.env.URL
-    : process.env.DEPLOY_PRIME_URL
-
 module.exports = withBundleAnalyzer(
   withMdxEnhanced({
     fileExtensions: ['mdx', 'md']
   })(
     withOptimizedImages({
       env: {
-        HOMEPAGE: BASE_URL,
+        HOMEPAGE: process.env.VERCEL_URL,
         NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID
       }
     })
