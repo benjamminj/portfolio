6e0cdb7554a76ee7cd70340ce8787890ee0dd13a
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2020-08-24 11:36:37 -0700

    Fix blog page links

--- tests/blog.test.e2e.ts
+++ tests/blog.test.e2e.ts
@@ -5,6 +5,6 @@ const BASE_URL = process.env.TEST_BASE_URL
 test('smoketest', async () => {
   await page.goto(`${BASE_URL}/blog`)
   await page.$('text="benjaminjohnson.me"')
-  const blogLinks = await page.$$('[href*="/blog/"]')
+  const blogLinks = await page.$$('[href^="/blog/"]')
   expect(blogLinks.length).toBeGreaterThan(0)
 })
