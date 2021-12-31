28c940c8b058ec419303853bcc4031f42968b53b
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2021-12-31 14:19:22 -0800

    chore: move writing directory (#161)
    * chore: move writing directory
    
    * fix: correct notes path

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,7 @@
 export const POSTS_DIRECTORY_NAME = 'writing'
 
 /** File path (relative to project root) of the writing directory */
-export const POSTS_BASE_PATH = `./${POSTS_DIRECTORY_NAME}/`
+export const POSTS_BASE_PATH = `./content/${POSTS_DIRECTORY_NAME}/`
+
+/** File path (relative to project root) of the notes directory */
+export const NOTES_BASE_PATH = `./content/notes`
