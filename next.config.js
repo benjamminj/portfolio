a499485e61be06737214dd4cb666688be44b7cc1
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-01-17 15:04:24 -0800

    deps: update deps, remove old ones (#163)

--- next.config.js
+++ next.config.js
@@ -1,27 +1,24 @@
-const withOptimizedImages = require('next-optimized-images')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer(
-  withOptimizedImages({
-    env: {
-      HOMEPAGE: process.env.URL || process.env.VERCEL_URL,
-      NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
-    },
-    async redirects() {
-      return [
-        {
-          source: '/blog',
-          destination: '/writing',
-          permanent: true,
-        },
-        {
-          source: '/blog/:path',
-          destination: '/:path',
-          permanent: true,
-        },
-      ]
-    },
-  })
-)
+module.exports = withBundleAnalyzer({
+  env: {
+    HOMEPAGE: process.env.URL || process.env.VERCEL_URL,
+    NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/blog',
+        destination: '/writing',
+        permanent: true,
+      },
+      {
+        source: '/blog/:path',
+        destination: '/:path',
+        permanent: true,
+      },
+    ]
+  },
+})
