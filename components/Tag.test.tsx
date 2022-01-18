c2652bdbdb22a6e75fc547fd1d1fba14ab9ae0e3
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-01-18 02:25:20 -0800

    fix: get unit tests passing

--- components/Tag.test.tsx
+++ components/Tag.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/preact'
 import { Tag } from './Tag'
 
 test('should render a tag with a link to the relevant tags page', () => {
