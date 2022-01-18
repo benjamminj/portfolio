c1a32064cfb4e9a1dd3281d43595ce5453722755
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-01-18 03:35:23 -0800

    deps: remove all cypress deps (#165)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['./test-utils/setupTests.ts'],
-  testPathIgnorePatterns: ['<rootDir>/e2e/', '<rootDir>/cypress/'],
+  testPathIgnorePatterns: ['<rootDir>/e2e/'],
   collectCoverageFrom: [
     '<rootDir>/{components,lib,pages,styles,src}/**/*.{ts,tsx,js,jsx}',
   ],
