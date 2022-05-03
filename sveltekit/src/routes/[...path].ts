e23f1d8f2c7fc2a4ff42ceeb7034d5e1a56d7dc2
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-05-03 12:22:03 -0700

    wip: setup rewrite routes??

--- sveltekit/src/routes/[...path].ts
+++ sveltekit/src/routes/[...path].ts
@@ -1,8 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = async ({ request, params }) => {
-	console.log('params >>', params);
-	const res = await fetch('https://benjaminjohnson.me/' + params.path);
+export const get: RequestHandler = async ({ params }) => {
+	const res = await fetch(import.meta.env.VITE_URL + params.path);
 	return new Response(res.body, {
 		...res
 	});
--- sveltekit/src/routes/_next/[...path].ts
+++ sveltekit/src/routes/_next/[...path].ts
@@ -1,8 +1,8 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = async ({ request, params }) => {
-	console.log('params >>', params);
-	const res = await fetch('https://benjaminjohnson.me/' + '_next/' + params.path);
+export const get: RequestHandler = async ({ params }) => {
+	const res = await fetch(import.meta.env.VITE_URL + '/_next/' + params.path);
+
 	return new Response(res.body, {
 		...res
 	});
