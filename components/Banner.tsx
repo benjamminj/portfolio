1e046f80820db3d7f65ec9b811f5ee49924ab6c9
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-05-03 13:45:09 -0700

    wip: try again?

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,10 @@ interface BannerProps {
 
 export const Banner = ({ children }: BannerProps) => {
   return (
-    <div className="flex items-end pt-48 pb-8 bg-gray-100 min-h-32 dark:bg-gray-900">
+    <div
+      className="flex items-end pt-48 pb-8 bg-gray-100 min-h-32 dark:bg-gray-900"
+      data-testid="potato"
+    >
       <div className="w-full px-4 mx-auto my-0 max-w-viewport md:max-w-prose">
         {children}
       </div>
