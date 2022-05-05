e18594309ea11e5402968b2ce7e749d7e4bcde9d
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-05-04 21:35:40 -0700

    fix: bad path

--- sveltekit/src/routes/[...path].ts
+++ sveltekit/src/routes/[...path].ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const get: RequestHandler = async ({ params }) => {
-	const res = await fetch(import.meta.env.VITE_URL + params.path);
+	const res = await fetch(import.meta.env.VITE_URL + '/' + params.path);
 	return new Response(res.body, {
 		...res
 	});
