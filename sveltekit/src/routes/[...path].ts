87ad3842e60c943a143d12e5e53509ea08501949
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-05-12 11:26:27 -0700

    chore: more tidies

--- sveltekit/src/routes/[...path].ts
+++ sveltekit/src/routes/[...path].ts
@@ -2,5 +2,7 @@ import type { RequestHandler } from '@sveltejs/kit'
 
 export const get: RequestHandler = async ({ params }) => {
 	const res = await fetch(import.meta.env.VITE_URL + '/' + params.path)
+	// `res` needs to be spread into a new object or else the serializer won't properly
+	// proxy the request as HTML.
 	return new Response(res.body, { ...res })
 }
--- sveltekit/src/routes/potato.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import HeaderLink from '$lib/components/header-link.svelte'
-</script>
-
-<HeaderLink title="Writing" href="/writing">writing</HeaderLink>
-<HeaderLink title="GitHub" href="https://github.com/benjamminj">github</HeaderLink>
-<HeaderLink title="Contact" href="mailto:benjamin.d.johnson@icloud.com">contact</HeaderLink>
