8ae9261d9ea68d69b36598d639ac17f5bd6f2cca
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-05-17 08:13:13 -0700

    fix: tsx syntax

--- sveltekit/src/lib/parse-markdown.ts
+++ sveltekit/src/lib/parse-markdown.ts
@@ -1,16 +1,44 @@
 import Prism from 'prismjs'
+import components from 'prismjs/components.json'
+// import components from 'prismjs/plugins/autoloader'
 // import loadLanguages from 'prismjs/components/index.js'
 import { Remarkable } from 'remarkable'
 
 const languages = new Map()
 
+import 'prismjs/components/prism-markup.js'
+import 'prismjs/components/prism-javascript.js'
+import 'prismjs/components/prism-jsx.js'
+import 'prismjs/components/prism-typescript.js'
+import 'prismjs/components/prism-tsx.js'
 // loadLanguages(['markup'])
 
-import(`prismjs/components/prism-javascript.js`)
-import(`prismjs/components/prism-markup.js`)
-import(`prismjs/components/prism-jsx.js`)
+// const loadLanguages = async (lang: string) => {
+// 	if (languages.has(lang)) {
+// 		return
+// 	}
+
+// 	const langFile = await import(`prismjs/components/prism-${lang}.js`)
+// 	// Prism.languages[lang] = langFile
+// 	languages.set(lang, langFile)
+// }
+// import(`prismjs/components/prism-javascript.js`)
+// import(`prismjs/components/prism-markup.js`)
+// import(`prismjs/components/prism-jsx.js`)
 // Prism.languages.extend()
 const md = new Remarkable({
+	highlight: (code, lang) => {
+		// return ''
+		// if (!languages.has(lang)) {
+		// 	// languages.set(lang, Prism.languages[lang])
+		// }
+		if (!lang) return ''
+
+		if (!Prism.languages[lang]) return ''
+
+		const highlighted = Prism.highlight(code, Prism.languages[lang], lang)
+		return highlighted
+	}
 	// highlight: (code, lang) => {
 	// 	if (!lang) return ''
 	// 	// TODO: fix issue w/ TSX parsing for vercel deploys...
@@ -22,8 +50,6 @@ const md = new Remarkable({
 	// 		languages.set(lang, true)
 	// 		// loadLanguages([lang])
 	// 	}
-	// 	const highlighted = Prism.highlight(code, Prism.languages[lang], lang)
-	// 	return highlighted
 	// }
 })
 
