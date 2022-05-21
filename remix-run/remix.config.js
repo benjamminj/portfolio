42412066e24fc656f34f4f01d30d7dbf9b974b9a
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-05-20 22:46:12 -0700

    feat: deploy to vercel

--- remix-run/remix.config.js
+++ remix-run/remix.config.js
@@ -2,9 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  serverBuildTarget:
+    process.env.NODE_ENV === 'development' ? undefined : 'vercel',
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-};
+}
