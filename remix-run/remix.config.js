eb957d4257e65bc1dcbe9d73aa62014decf75fd4
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-05-20 22:49:24 -0700

    fix: update build targetz

--- remix-run/remix.config.js
+++ remix-run/remix.config.js
@@ -2,9 +2,16 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget:
-    process.env.NODE_ENV === 'development' ? undefined : 'vercel',
+  serverBuildTarget: 'vercel',
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  // server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['**/.*'],
+  // appDirectory: 'app',
+  // browserBuildDirectory: 'public/build',
+  // publicPath: '/build/',
+  // serverBuildDirectory: 'api/_build',
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
