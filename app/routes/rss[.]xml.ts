e8815345cb59a755bb76daada88f56f831b05e2c
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-05-29 13:47:56 -0700

    fix: html escaping

--- app/routes/rss[.]xml.ts
+++ app/routes/rss[.]xml.ts
@@ -33,7 +33,7 @@ export const loader: LoaderFunction = async ({ request }) => {
   const getItemXML = (post: typeof posts[0]) => {
     // TODO: this should be the whole dang post, not just the description.
     const description = post.description
-      ? `<description><![CDATA[${escapeCdata(escapeHtml(post.html))}]]></description>` // prettier-ignore
+      ? `<description><![CDATA[${escapeCdata(post.html)}]]></description>` // prettier-ignore
       : ''
 
     return `
