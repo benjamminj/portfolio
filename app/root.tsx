a6e796e90096287123db158f10c3da30366bd43f
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2022-06-03 16:20:05 -0700

    feat: add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -20,7 +20,7 @@ export const links: LinksFunction = () => {
     { rel: 'stylesheet', href: styles },
     {
       rel: 'icon',
-      href: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌀</text></svg>`,
+      href: `/favicon.svg`,
     },
   ]
 }
