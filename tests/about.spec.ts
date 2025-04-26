695e3ecc4a1ecc226b46a337736f79ce126b8610
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2025-04-25 22:17:11 -0700

    feat: new about page

--- tests/about.spec.ts
+++ tests/about.spec.ts
@@ -1,22 +1,14 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("/about", () => {
-	test("should display the about page", async ({ page }) => {
-		await page.goto("/about");
-		const $title = page.locator("text=benjamin johnson").first();
-		expect(await $title.isVisible()).toEqual(true);
+test("should display the about page", async ({ page }) => {
+	await page.goto("/about");
+	const $title = page.locator("text=benjamin johnson").first();
+	expect(await $title.isVisible()).toEqual(true);
 
-		const navigationLinks = [
-			["./writing", "/writing"],
-			["./about", "/about"],
-		];
-
-		await Promise.all(
-			navigationLinks.map(async ([text, href]) => {
-				const $element = page.locator(`text=${text}`);
-				expect(await $element.isVisible()).toEqual(true);
-				expect(await $element.getAttribute("href")).toEqual(href);
-			}),
-		);
+	const $heading = await page.getByRole("heading", {
+		level: 1,
+		name: "about me",
 	});
+
+	await expect(await $heading).toBeVisible();
 });
