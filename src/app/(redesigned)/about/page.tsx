a663f6848815c784b8542d756b49cd0bf5da3231
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2025-04-26 13:56:16 -0700

    feat: new about page (#219)

---
+++ src/app/(redesigned)/about/page.tsx
@@ -0,0 +1,17 @@
+import { H } from "@/lib/h";
+import { Markdown } from "@/lib/markdown";
+import { readFile } from "@/lib/read-file";
+
+export default async function AboutRoute() {
+	const raw = await readFile("about.md");
+
+	return (
+		<>
+			<H level={1} className="pb-line">
+				about me
+			</H>
+
+			<Markdown raw={raw} __flushEdges />
+		</>
+	);
+}
