a2c4869539154c8e40faeb7d5ec1dd8d7e3435d8
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2026-06-07 22:13:38 -0700

    refaactor(sveltekit): basic layout + some components (#240)

---
+++ apps/website-sv/src/lib/background-code-pattern.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { cn } from "./cn";
+	import { getBackgroundCodePatternText } from "./get-background-code-pattern-text";
+
+	let innerWidth = $state(0);
+	let innerHeight = $state(0);
+
+	const { text, reps } = $derived(
+		getBackgroundCodePatternText({
+			height: innerHeight,
+			width: innerWidth,
+		}),
+	);
+</script>
+
+<svelte:window bind:innerWidth bind:innerHeight />
+
+<div
+	class={cn(
+		"absolute inset-0 overflow-hidden break-all transition-colors duration-500",
+		reps > 0 ? "text-fg/4" : "text-transparent",
+	)}
+>
+	{text}
+</div>
--- apps/website-sv/src/routes/+layout.svelte
+++ apps/website-sv/src/routes/+layout.svelte
@@ -1,9 +1,47 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.svg";
+	import BackgroundCodePattern from "$lib/background-code-pattern.svelte";
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="font-mono bg-bg text-fg text-body min-h-screen relative">
+	<BackgroundCodePattern />
+
+	{@render children()}
+</div>
+
+<style>
+	/* jetbrains-mono-latin-wght-normal */
+	@font-face {
+		font-family: "JetBrains Mono Variable";
+		font-style: normal;
+		font-display: swap;
+		font-weight: 100 800;
+		src: url(@fontsource-variable/jetbrains-mono/files/jetbrains-mono-latin-wght-normal.woff2)
+			format("woff2-variations");
+		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+			U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191,
+			U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+	}
+
+	/* jetbrains-mono-latin-wght-italic */
+	@font-face {
+		font-family: "JetBrains Mono Variable";
+		font-style: italic;
+		font-display: swap;
+		font-weight: 100 800;
+		src: url(@fontsource-variable/jetbrains-mono/files/jetbrains-mono-latin-wght-italic.woff2)
+			format("woff2-variations");
+		unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+			U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+20AC, U+2122, U+2191,
+			U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+	}
+
+	:global(:root) {
+		--font-jetbrains-mono: "JetBrains Mono Variable", monospace;
+	}
+</style>
